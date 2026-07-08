d9b8f97164d687f7cd1b4f4d425873e3e88b86fc
AliEmreBulut <aliemrebulut_2001@hotmail.com>
2026-07-08 15:24:39 +0300

    Proje konfigürasyonları ve paket bağımlılıkları güncellendi

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
--- frontend/src/components/contact/types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export interface SocialLink {
-  id: string;
-  href: string;
-  label: string;
-  value: string;
-  icon: ReactNode;
-}
